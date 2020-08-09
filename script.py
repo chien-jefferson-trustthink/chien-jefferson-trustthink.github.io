dd75798c7436218f5479ade1dd873e52b4164a00
jkchien <jkchien@ucsd.edu>
2020-08-09 04:04:09 -0700

    testing

---
+++ script.py
@@ -0,0 +1,34 @@
+import json
+import csv
+
+latitude = []
+longtitude = []
+geo_data = []
+with open('./data_log.csv') as data_log:
+    reader = csv.reader(data_log, delimiter=',')
+    
+    for row in reader:
+        longtitude.append(row[0])
+        latitude.append(row[1])
+
+latitude.remove("latitude")
+longtitude.remove("longtitude")
+
+for i in range(len(latitude)):
+    temp = {
+        str("lat"): float(latitude[i]),
+        str("lng"): float(longtitude[i])
+    }   
+
+    geo_data.append(temp)
+
+
+geo_data_json = json.dumps(geo_data)
+
+
+print(geo_data_json)
+
+with open("data_log.json", "w") as outfile:
+    json.dump(geo_data, outfile)
+
+        
