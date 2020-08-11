5ee05497e9d4a8b2be2e21c524f3d89d32d5d14e
jkchien <jkchien@ucsd.edu>
2020-08-11 02:25:35 -0700

    Versioning

--- script.py
+++ script.py
@@ -26,9 +26,42 @@ for i in range(len(latitude)):
 geo_data_json = json.dumps(geo_data)
 
 
+
 print(geo_data_json)
 
 with open("data_log.json", "w") as outfile:
     json.dump(geo_data, outfile)
 
+
+
+latitude = []
+longtitude = []
+geo_data = []
+with open('./violation.csv') as data_log:
+    reader = csv.reader(data_log, delimiter=',')
+    
+    for row in reader:
+        longtitude.append(row[0])
+        latitude.append(row[1])
+
+latitude.remove("latitude")
+longtitude.remove("longtitude")
+
+for i in range(len(latitude)):
+    temp = {
+        str("lat"): float(latitude[i]),
+        str("lng"): float(longtitude[i])
+    }   
+
+    geo_data.append(temp)
+
+
+geo_data_json = json.dumps(geo_data)
+
+
+
+print(geo_data_json)
+
+with open("violation.json", "w") as outfile:
+    json.dump(geo_data, outfile)
         
